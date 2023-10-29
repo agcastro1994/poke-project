5986491b556b877962361a2ce004e5c8b272b489
Arturo Castro <agcastro1994@gmail.com>
2023-10-29 15:58:06 -0300

    improvements to the cards views and the filter function

--- poke-project-app/src/components/PokeSearch.tsx
+++ poke-project-app/src/components/PokeSearch.tsx
@@ -4,37 +4,32 @@ import charizard from '../images/charizards.svg';
 import PokemonFilterService from '../services/PokeFilterService';
 import { PokemonSmall } from '../interfaces/Pokemon/PokemonSmall';
 import PokeCard from './PokeCard';
+import SearchBar from './SearchBar';
 
 function PokeSearch() { 
 
-    const getPokemon = (e: React.FormEvent<HTMLFormElement>, id: number) => {
-        e.preventDefault()
-        if(region !== ""){
-            PokemonFilterService.getPokemonByRegion(region).then(pokeList => {
+    const getPokemon = () => {
+        if(!isNaN(Number(search))){
+            PokemonFilterService.getPokemon(Number(search)).then(poke => {
+                const pokeList: PokemonSmall[] = [poke]
                 setPokemon(pokeList)
             })
+
         }
         else{
-            PokemonFilterService.getPokemon(id).then(poke => {
-                const pokeList: PokemonSmall[] = [poke]
+            PokemonFilterService.getPokemonByRegion(search).then(pokeList => {
                 setPokemon(pokeList)
             })
         }
         
     }
 
-    const [value, setValue] = useState<number>(1);
-    const [region, setRegion] = useState<string>("")
+    const [search, setSearch] = useState<string>("")
     const [pokemon, setPokemon] = useState<PokemonSmall[]>([]);
 
-    const onNumberChange = (e: ChangeEvent<HTMLInputElement>) => {
-        const value =  e.target.valueAsNumber;
-        setValue(value);
-    }
-
-    const onRegionChange = (e: ChangeEvent<HTMLInputElement>) => {
-        const regionSelected =  e.target.value;
-        setRegion(regionSelected);
+    const onInputChange = (input: string) => {
+        const searchValue = input;
+        setSearch(searchValue);
     }
 
     return (
@@ -44,26 +39,10 @@ function PokeSearch() {
             <p style={{marginTop: "1.5em"}}>
             Poke project 
             </p>
-            <form onSubmit={(e)=>getPokemon(e,value)}>
-                <label htmlFor="pokedexNumber">Pokedex Number:</label>
-                <input
-                disabled={region !== ""}
-                min={0} 
-                id={"pokedexNumber"}               
-                type="number"
-                value={value ?? 1}
-                onChange={onNumberChange}                
-                />
-                <label htmlFor="region">Region:</label>
-                <input 
-                id={"region"}                      
-                type="text"
-                value={region}
-                onChange={onRegionChange}
-                />
 
-                <button type="submit">Mandale mecha capo</button>
-            </form>
+            <SearchBar onChange={(input: string) => onInputChange(input)} onSearch={()=> getPokemon()} value={search}/>
+
+            
             
             {pokemon.length > 0 && 
                 pokemon.length === 1 ?
